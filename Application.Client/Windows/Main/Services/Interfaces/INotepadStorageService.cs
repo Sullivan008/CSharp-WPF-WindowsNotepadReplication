14226eed296014d433df8bc11fbf585c664df373
Sullivan008 <kozakpeter0424@gmail.com>
2021-04-20 21:01:50 +0200

    * The following methods and properties have been added to NotepadStorageService:   * UsedFilePath: Property which returns the path of the file in use.   * UsedFileName: Property which returns the name of the file in use.   * HasUsedFile: Property that returns true if there is a file in use.   * SetUsedFilePath: Method that sets the path to the file used.

--- Application.Client/Windows/Main/Services/Interfaces/INotepadStorageService.cs
+++ Application.Client/Windows/Main/Services/Interfaces/INotepadStorageService.cs
@@ -1,5 +1,13 @@
 ﻿namespace Application.Client.Windows.Main.Services.Interfaces
 {
     public interface INotepadStorageService
-    { }
+    {
+        string UsedFilePath { get; }
+
+        string UsedFileName { get; }
+
+        bool HasUsedFile { get; }
+
+        void SetUsedFilePath(string filePath);
+    }
 }
--- Application.Client/Windows/Main/Services/NotepadStorageService.cs
+++ Application.Client/Windows/Main/Services/NotepadStorageService.cs
@@ -1,7 +1,30 @@
-﻿using Application.Client.Windows.Main.Services.Interfaces;
+﻿using System;
+using System.IO;
+using Application.Client.Windows.Main.Services.Interfaces;
 
 namespace Application.Client.Windows.Main.Services
 {
     public class NotepadStorageService : INotepadStorageService
-    { }
+    {
+        public string UsedFilePath { get; private set; }
+
+        public string UsedFileName => Path.GetFileNameWithoutExtension(UsedFilePath);
+
+        public bool HasUsedFile => !string.IsNullOrWhiteSpace(UsedFilePath);
+
+        public void SetUsedFilePath(string filePath)
+        {
+            if (string.IsNullOrWhiteSpace(filePath))
+            {
+                throw new ArgumentNullException(nameof(filePath), @"The value cannot be null!");
+            }
+
+            if (!File.Exists(filePath))
+            {
+                throw new FileNotFoundException("Could not find file!", filePath);
+            }
+
+            UsedFilePath = filePath;
+        }
+    }
 }
