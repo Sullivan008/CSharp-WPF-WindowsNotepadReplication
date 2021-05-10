ccd57f7ba4597866621a20238c7acd396782ffcb
Sullivan008 <kozakpeter0424@gmail.com>
2021-05-10 20:31:58 +0200

    * Created the ExitCommand.   * This command close the application.   * If the contents of the input field have not yet been saved, that it is possible to save it.

---
+++ Application.Client/Windows/Main/Commands/ExitCommand.cs
@@ -0,0 +1,60 @@
+﻿using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using Application.Client.Core.Commands;
+using Application.Client.Core.Dialogs.MessageDialog.Enums;
+using Application.Client.Core.Dialogs.MessageDialog.Models;
+using Application.Client.Core.Dialogs.SaveFileDialog.Enums;
+using Application.Client.Core.Dialogs.SaveFileDialog.Models;
+using Application.Client.Core.Services.FileWriter.Models;
+
+namespace Application.Client.Windows.Main.ViewModels
+{
+    public partial class MainWindowViewModel
+    {
+        private ICommand _exitCommand;
+
+        public ICommand ExitCommand => _exitCommand ??= new RelayCommandAsync(ExitCommandExecute);
+
+        private async Task ExitCommandExecute()
+        {
+            if (_notepadStorageService.HasDocumentModified)
+            {
+                MessageDialogResult messageDialogResult = await _messageDialogService.ShowDialogAsync(
+                    new MessageDialogOptions { Title = "Notepad", Content = $"Do you want to save the {_notepadStorageService.UsedFileNameWithExtension} changes?", Button = MessageBoxButton.YesNoCancel });
+
+                switch (messageDialogResult.MessageDialogResultType)
+                {
+                    case MessageDialogResultType.Yes:
+                        {
+                            if (_notepadStorageService.HasUsedFile)
+                            {
+                                await _textFileWriterService.WriteAsync(new WriteTextFileModel { FilePath = _notepadStorageService.UsedFilePath, Content = _content });
+                            }
+                            else
+                            {
+                                SaveFileDialogResult saveFileDialogResult = await _saveFileDialogService.ShowDialogAsync(new SaveFileDialogOptions { FileFilters = GetSaveFileDialogFilters() });
+
+                                if (saveFileDialogResult.SaveFileDialogResultType == SaveFileDialogResultType.Ok)
+                                {
+                                    await _textFileWriterService.WriteAsync(new WriteTextFileModel { FilePath = saveFileDialogResult.SavedFilePath, Content = _content });
+                                }
+                                else if (saveFileDialogResult.SaveFileDialogResultType == SaveFileDialogResultType.Cancel)
+                                {
+                                    return;
+                                }
+                            }
+
+                            break;
+                        }
+                    case MessageDialogResultType.Cancel:
+                    {
+                        return;
+                    }
+                }
+            }
+
+            System.Windows.Application.Current.Shutdown();
+        }
+    }
+}
