104d8dffd6a33d6f7696ff6686ef5fcad3f7ab9a
Sullivan008 <kozakpeter0424@gmail.com>
2021-05-14 18:45:54 +0200

    * Created the generic RealyCommandAsync<TCommandParameter> class.

--- Application.Client/Core/Commands/RelayCommandAsync.cs
+++ Application.Client/Core/Commands/RelayCommandAsync.cs
@@ -65,4 +65,66 @@ namespace Application.Client.Core.Commands
             }
         }
     }
+
+    public class RelayCommandAsync<TCommandParameter> : ICommand
+    {
+        private readonly Func<Task> _execute;
+
+        private readonly Action<TCommandParameter> _executeParam;
+
+        private readonly Predicate<TCommandParameter> _canExecute;
+
+        private bool _isExecuting;
+
+        public RelayCommandAsync(Func<Task> execute) : this(execute, null)
+        { }
+
+        public RelayCommandAsync(Action<TCommandParameter> executeParam)
+        {
+            _executeParam = executeParam;
+        }
+
+        public RelayCommandAsync(Func<Task> execute, Predicate<TCommandParameter> canExecute)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            if (!_isExecuting && _canExecute == null)
+            {
+                return true;
+            }
+
+            return !_isExecuting && _canExecute((TCommandParameter)parameter);
+        }
+
+        public async void Execute(object parameter)
+        {
+            _isExecuting = true;
+
+            try
+            {
+                if (_execute != null)
+                {
+                    await _execute();
+                }
+                else
+                {
+                    _executeParam((TCommandParameter)parameter);
+                }
+            }
+            finally
+            {
+                _isExecuting = false;
+            }
+        }
+    }
 }
