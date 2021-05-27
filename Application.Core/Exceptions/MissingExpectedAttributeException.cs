cc357ea9a9f323b3cf386a695b85ee6a8e46f898
Sullivan008 <kozakpeter0424@gmail.com>
2021-05-27 19:23:12 +0200

    * Created an EnumExtension class.   * Implemented the ToEnumMemberAttributeValue() method into this extension. This method return the value from Enum Member Attribute.   * Implemented the MissingExpectedAttribute Exception class.

---
+++ Application.Core/Exceptions/MissingExpectedAttributeException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Application.Core.Exceptions
+{
+    public class MissingExpectedAttributeException : Exception
+    {
+        public MissingExpectedAttributeException()
+        { }
+
+        public MissingExpectedAttributeException(string message) : base(message)
+        { }
+
+        public MissingExpectedAttributeException(string message, Exception innerException) : base(message, innerException)
+        { }
+    }
+}
---
+++ Application.Core/Extensions/EnumExtension.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using Application.Core.Exceptions;
+
+namespace Application.Core.Extensions
+{
+    public static class EnumExtension
+    {
+        public static string ToEnumMemberAttrValue<TEnum>(this TEnum value) where TEnum : Enum
+        {
+            EnumMemberAttribute enumMemberAttr = GetAttribute<EnumMemberAttribute>(value);
+
+            string enumMemberAttrValue = enumMemberAttr.Value;
+
+            return !string.IsNullOrWhiteSpace(enumMemberAttrValue) ? enumMemberAttrValue : throw new ArgumentNullException(nameof(value), "The value cannot be null!");
+        }
+
+        private static TAttributeType GetAttribute<TAttributeType>(this Enum value)
+        {
+            Type type = value.GetType();
+            MemberInfo[] memberInfo = type.GetMember(value.ToString());
+
+            object[] attributes = memberInfo[0].GetCustomAttributes(typeof(TAttributeType), false);
+            object attribute = attributes.SingleOrDefault();
+
+            if (attribute == null)
+            {
+                throw new MissingExpectedAttributeException($"The following attribute is missing! {nameof(TAttributeType).ToUpper()}: {typeof(TAttributeType)}");
+            }
+
+            return (TAttributeType)attribute;
+        }
+    }
+}
