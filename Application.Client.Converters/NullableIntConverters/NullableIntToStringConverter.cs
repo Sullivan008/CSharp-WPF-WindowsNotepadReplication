0faa7489d64d6ed535f87ff0de7e4ea0869cadb9
Sullivan008 <kozakpeter0424@gmail.com>
2021-07-12 22:21:46 +0200

    * Created the Application.Client.Converters project. This project contains every converter that implement the IValueConverter interface.   * Created the NullableIntToString converter.

---
+++ Application.Client.Converters/NullableIntConverters/NullableIntToStringConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Application.Client.Converters.NullableIntConverters
+{
+    public class NullableIntToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int? integer = (int?)value;
+
+            return integer?.ToString() ?? (object)string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return !int.TryParse((string)value, out int result) ? default(int?) : result;
+        }
+    }
+}
