79a561cc3c5d3f2518cf26ca0be3c4344ebd48b2
Sullivan008 <kozakpeter0424@gmail.com>
2021-08-13 22:07:19 +0200

    * Created the necessary class with interface for ReplaceDialog (is just an initial state).

---
+++ Application.Client.Dialogs/ReplaceDialog/Interfaces/IReplaceDialog.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Application.Client.Dialogs.ReplaceDialog.Interfaces
+{
+    public interface IReplaceDialog
+    {
+        Task ShowAsync();
+    }
+}
---
+++ Application.Client.Dialogs/ReplaceDialog/ReplaceDialog.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Application.Client.Dialogs.ReplaceDialog.Interfaces;
+
+namespace Application.Client.Dialogs.ReplaceDialog
+{
+    public class ReplaceDialog : IReplaceDialog
+    {
+        public Task ShowAsync()
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
